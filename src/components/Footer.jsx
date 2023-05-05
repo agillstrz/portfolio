132084f4ecce2cd9f6bddc3c0cc16df40af81d98
agillstrz <agillstrz@gmail.com>
2023-05-05 11:12:33 +0700

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,7 @@
 function Footer() {
   return (
     <div className="lg:mt-0 lg:px-40  px-4 py-5 lg:py-8  lg:h-20 justify-center  w-full dark:bg-main  border-t-2 dark:border-dark2 border-light1 bg-light2   flex flex-col items-center">
-      <span className="lg:text-lg text-md pt-2">
-        2023 © all rights reserved
-      </span>
+      <span className="lg:text-lg text-md pt-2">2023 © agillstrz</span>
     </div>
   );
 }
